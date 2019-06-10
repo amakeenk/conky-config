fa41fee6e19549349bced59855099d4a08a3fac8
Alexander Makeenkov <amakeenk@altlinux.org>
2019-06-10 21:05:44 +0300

    show current version of apps (vscode, etc)

--- scripts/check_update_yandex_browser.sh
+++ scripts/check_update_yandex_browser.sh
@@ -2,5 +2,5 @@
 
 last_rpm=$(curl -s https://repo.yandex.ru/yandex-browser/rpm/beta/x86_64/ | grep yandex-browser-beta | grep rpm | cut -d">" -f2 | cut -d"<" -f1)
 last_vers=$(echo ${last_rpm} | cut -d"-" -f4)
-current_vers=$(rpm -qi yandex-browser-beta | grep Version | tr -d [:alpha:]":"" ")
-[ "${last_vers}" == "${current_vers}" ] && echo "None" || echo "Yes"
+current_vers=$(rpm -q yandex-browser-beta --qf '%{VERSION}')
+[ "${last_vers}" == "${current_vers}" ] && echo "None [Current version: ${current_vers}]" || echo "Yes [New Version: ${last_vers}]"
--- scripts/check_update_yandex_disk.sh
+++ scripts/check_update_yandex_disk.sh
@@ -2,5 +2,5 @@
 
 last_rpm=$(curl -s https://repo.yandex.ru/yandex-disk/rpm/stable/x86_64/ | grep yandex-disk | grep "\.rpm" | cut -d">" -f2 | cut -d"<" -f1)
 last_vers=$(echo ${last_rpm} | cut -d"-" -f3)
-current_vers=$(rpm -qi yandex-disk | grep Version | tr -d [:alpha:]":"" ""()")
-[ "${last_vers}" == "${current_vers}" ] && echo "None" || echo "Yes"
+current_vers=$(rpm -q yandex-disk --qf '%{VERSION}')
+[ "${last_vers}" == "${current_vers}" ] && echo "None [Current version: ${current_vers}]" || echo "Yes [New Version: ${last_vers}]"
