a37406fddb9a6c50b8f441d16384d92bc395e32e
Alexander Makeenkov <amakeenk@altlinux.org>
2019-06-12 17:42:47 +0300

    renamed run_conky.sh -> run-conky.sh and enabled debug mode

---
+++ run-conky.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -e
+# Simple script for running conky with reloading support
+# Author: Alexander Makeenkov <whoami.tut@gmail.com>
+
+[ "$1" == "reload" ] && killall conky
+
+conky --debug >>${TMP}/conky.log 2>&1 &
+
--- run_conky.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -e
-
-is_reload="$1"
-
-[ "${is_reload}" == "reload" ] && killall conky
-
-conky >>${TMP}/conky.log 2>&1 &
