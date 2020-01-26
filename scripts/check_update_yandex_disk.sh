c15ad67d6328148c22f6d25cca3d0ebc5e9ab280
Alexander Makeenkov <amakeenk@altlinux.org>
2020-01-26 18:33:18 +0300

    drop unnecessary scripts

--- scripts/check_update_yandex_disk.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-last_rpm=$(curl -s https://repo.yandex.ru/yandex-disk/rpm/stable/x86_64/ | grep yandex-disk | grep "\.rpm" | cut -d">" -f2 | cut -d"<" -f1)
-last_vers=$(echo ${last_rpm} | cut -d"-" -f3)
-current_vers=$(rpm -q yandex-disk --qf '%{VERSION}')
-[ "${last_vers}" == "${current_vers}" ] && echo "None [Current version: ${current_vers}]" || echo "Yes [New Version: ${last_vers}]"
