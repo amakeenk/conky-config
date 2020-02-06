70f57aa51c8a11ae70af6fb2e83bc1a40c91090a
Alexander Makeenkov <amakeenk@altlinux.org>
2020-02-06 14:41:40 +0300

    update run-conky.sh

--- run-conky.sh
+++ run-conky.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 # Simple script for running conky with reloading support
-# Author: Alexander Makeenkov <whoami.tut@gmail.com>
 
 [ "$1" == "reload" ] && killall conky
-
-conky --debug >>${TMP}/conky.log 2>&1 &
-
+conky --daemonize &>/dev/null
