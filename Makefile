3c79a81a011a0c842b98057cae4f52dcb9bc1f28
Alexander Makeenkov <amakeenk@altlinux.org>
2022-10-08 02:25:28 +0300

    upd

--- Makefile
+++ Makefile
@@ -3,6 +3,5 @@ BINDIR ?= $(PREFIX)/bin
 CONKYDIR ?= /etc/conky
 
 install:
-	mkdir -p $(CONKYDIR)/scripts
 	install -m0755 run-conky.sh $(BINDIR)/run-conky
 	install -m0644 conky.conf $(CONKYDIR)/conky.conf
--- run-conky.sh
+++ run-conky.sh
@@ -2,5 +2,9 @@
 # Simple script for running conky with reloading support
 
 [ "$1" == "reload" ] && killall conky
+
 conky --daemonize &>/dev/null
-conky --daemonize -x 1920 &>/dev/null
+
+if xrandr --listmonitors | grep 'Monitors: 2' >/dev/null; then
+    conky --daemonize -x 1930 &>/dev/null
+fi
